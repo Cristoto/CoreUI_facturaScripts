0bd88ee4ea283156c6b78dc46f307f91cc025543
Cristo Manuel Estévez Hernández <cristom.estevez@gmail.com>
2018-04-11 11:09:31 +0100

    Modified logo and effect alert

---
+++ Lib/MenuItem.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FacturaScripts\Plugins\CoreUI_template\Lib;
+
+/**
+ * Structure for each of the items in the CoreUI menu.
+ *
+ * @author Carlos García Gómez <carlos@facturascripts.com>
+ * @author Artex Trading sa <jcuello@artextrading.com>
+ * @author Cristo Manuel Estévez Hernández <cristom.estevez@gmail.com>
+ */
+class MenuItem extends \FacturaScripts\Core\Lib\MenuItem
+{
+    
+}
