2bdbbdc498d837ae35db8c8b081ab2aacffc7814
Cristo Manuel Estévez Hernández <cristom.estevez@gmail.com>
2018-04-13 08:43:24 +0100

    Modifed name plugin

--- Lib/MenuItem.php
+++ Lib/MenuItem.php
@@ -18,7 +18,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-namespace FacturaScripts\Plugins\CoreUI_template\Lib;
+namespace FacturaScripts\Plugins\CoreuiTemplate\Lib;
 
 /**
  * Structure for each of the items in the FacturaScripts menu.
